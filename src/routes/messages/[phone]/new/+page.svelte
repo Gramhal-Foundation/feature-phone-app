b528898957d48f0a2bd9f49a2a9ed68c8d4681cd
akashrajum-gramhal <akash@gramhal.org>
2022-12-29 15:22:02 +0530

    add recording indicator

--- src/routes/messages/[phone]/new/+page.svelte
+++ src/routes/messages/[phone]/new/+page.svelte
@@ -3,7 +3,30 @@
 
 	import SoftwareKeys from '$lib/components/SoftwareKeys.svelte';
 
-	let recoding = false;
+	let recording = false;
+	let time = 60;
+
+	const handleKeyDown = (event: KeyboardEvent) => {
+		switch (event.key) {
+			case 'Enter':
+				recording = !recording;
+				if (recording) {
+					const interval = setInterval(() => {
+						time--;
+						if (time === 0) {
+							clearInterval(interval);
+							recording = false;
+							time = 60;
+						}
+					}, 1000);
+				} else {
+					time = 60;
+				}
+				return;
+			default:
+				return;
+		}
+	};
 </script>
 
 <main class="flex h-screen flex-col bg-primary pb-7">
@@ -12,13 +35,20 @@
 			<img src="/uttrr.svg" alt="logo" class="h-16 w-16" />
 			<h1 class="text-center text-lg font-semibold text-white">{$page.params.phone}</h1>
 		</div>
-		{#if recoding}
-			<img src="/mic.svg" alt="mic" class="h-16 w-16" />
+		{#if recording}
+			<img src="/mic.svg" alt="mic" class="h-12 w-12 animate-pulse" />
+			<span class="text-center text-white">{time}</span>
 		{/if}
 	</section>
 	<SoftwareKeys>
 		<div slot="center" class="flex items-center justify-center">
-			<img class="h-4 w-4" src="/mic.svg" alt="ok" />
+			{#if recording}
+				<img class="h-4 w-4" src="/tick.svg" alt="finish" />
+			{:else}
+				<img class="h-4 w-4" src="/mic.svg" alt="record" />
+			{/if}
 		</div>
 	</SoftwareKeys>
 </main>
+
+<svelte:window on:keydown={handleKeyDown} />
