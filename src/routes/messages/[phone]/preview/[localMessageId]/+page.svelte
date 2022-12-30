1cae137ed63eb98fef0f27bfda97e0a4e69c2ca8
akashrajum-gramhal <akash@gramhal.org>
2022-12-30 12:23:02 +0530

    fetch audio from indexdb and play it

--- src/routes/messages/[phone]/preview/[localMessageId]/+page.svelte
+++ src/routes/messages/[phone]/preview/[localMessageId]/+page.svelte
@@ -1,9 +1,102 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	import SoftwareKeys from '$lib/components/SoftwareKeys.svelte';
+	import { goto } from '$app/navigation';
 
+	let player: HTMLAudioElement | null = null;
 	let playing = false;
+	let audioBlob: Blob | null = null;
+	let time = '0:0 / 0:0';
+
+	onMount(() => {
+		// Get the audio blob from messages collection in uttrr db in indexdb
+		let db: IDBDatabase;
+		const request = indexedDB.open('uttrr', 1);
+		request.onerror = (event) => {
+			console.error("Why didn't you allow my web app to use IndexedDB?!");
+		};
+
+		request.onsuccess = (event: any) => {
+			db = event.target.result;
+			const transaction = db.transaction(['messages'], 'readonly');
+			const objectStore = transaction.objectStore('messages');
+			const request = objectStore.get($page.params.localMessageId);
+			request.onerror = (event) => {
+				console.error('Error getting audio blob from IndexedDB');
+			};
+			request.onsuccess = (event: any) => {
+				audioBlob = event.target.result.audio;
+				if (!audioBlob) return;
+				console.log(audioBlob);
+				const audioUrl = URL.createObjectURL(audioBlob);
+				player = document.querySelector('audio');
+				if (!player) {
+					return;
+				} else {
+					player.src = audioUrl;
+					player.onpause = () => {
+						playing = false;
+					};
+					player.onplay = () => {
+						playing = true;
+					};
+					player.onloadedmetadata = () => {
+						const duration = player?.duration || 0;
+						const minutes = Math.floor(duration / 60);
+						const seconds = Math.floor(duration % 60);
+						time = `0:0 / ${minutes}:${seconds}`;
+					};
+					player.ontimeupdate = () => {
+						const currentTime = player?.currentTime || 0;
+						const minutes = Math.floor(currentTime / 60);
+						const seconds = Math.floor(currentTime % 60);
+						time = `${minutes}:${seconds} / ${time.split('/')[1]}`;
+					};
+				}
+			};
+		};
+
+		return () => {
+			if (player) {
+				player.pause();
+				player = null;
+			}
+		};
+	});
+
+	const handleKeyDown = (event: KeyboardEvent) => {
+		switch (event.key) {
+			case 'ArrowLeft':
+			case 'SoftLeft':
+				// Back
+				if (player && playing) {
+					player.pause();
+					player.currentTime = 0;
+				}
+				goto(`/messages/${$page.params.phone}/new`);
+				break;
+			case 'Enter':
+				// Play/Pause
+				if (player) {
+					if (playing) {
+						player.pause();
+					} else {
+						player.play();
+					}
+				}
+				break;
+			case 'ArrowRight':
+			case 'SoftRight':
+				// Send
+				if (player && playing) {
+					player.pause();
+					player.currentTime = 0;
+				}
+				break;
+		}
+	};
 </script>
 
 <main class="flex h-screen flex-col bg-primary pb-7">
@@ -14,6 +107,8 @@
 		</div>
 
 		<img src="/speaker.svg" alt="speaker" class="h-12 w-12" class:animate-pulse={playing} />
+
+		<span class="text-white">{time}</span>
 	</section>
 	<SoftwareKeys>
 		<div slot="left" class="flex items-center justify-start">
@@ -31,3 +126,7 @@
 		</div>
 	</SoftwareKeys>
 </main>
+
+<audio class="invisible" />
+
+<svelte:window on:keydown={handleKeyDown} />
