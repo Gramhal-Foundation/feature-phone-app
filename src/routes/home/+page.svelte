983a712d9c9bf34b33219b95c4ad1a915bb3bd36
akashrajum-gramhal <akash@gramhal.org>
2023-01-16 15:44:34 +0530

    add home translations

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import Navigation from '$lib/utils/navigation';
-	import { onMount } from 'svelte';
-
+	import { onMount, tick } from 'svelte';
 	import ContactComponent from '$lib/components/ContactComponent.svelte';
 	import SoftwareKeys from '$lib/components/SoftwareKeys.svelte';
 	import axiosInstance from '$lib/utils/axios';
 	import toast from 'svelte-french-toast';
-
-	let recentConversations: Message[] = [];
-	let userMessageCount: number = 0;
-	let usersPhone: string = '7760032885';
-	let userMessageDateTime: string | null = null;
+	import { getLocale } from '$lib/utils';
 
 	interface Message {
 		max_timestamp: any;
@@ -25,6 +20,19 @@
 	interface HomeMessages {
 		messages: Message[];
 	}
+
+	interface Translations {
+		recent: string;
+		contacts: string;
+		messagesFetchError: string;
+	}
+
+	let recentConversations: Message[] = [];
+	let userMessageCount: number = 0;
+	let usersPhone: string = '7760032885';
+	let userMessageDateTime: string | null = null;
+	let pageLoading: boolean = true;
+	let translations: Translations | null = null;
 
 	const fetchRecentConversations = async () => {
 		try {
@@ -42,12 +50,29 @@
 			recentConversations = data.messages;
 		} catch (error) {
 			console.log(error);
-			toast.error('Error fetching recent conversations');
+			toast.error(translations?.messagesFetchError ?? 'Error fetching recent conversations');
 		}
 	};
 
 	onMount(() => {
-		Navigation.selectFirstElement();
+		// Set translations
+		const lang = getLocale();
+		pageLoading = true;
+		axiosInstance
+			.get(`translations/${lang}/home`)
+			.then((res) => {
+				translations = res.data;
+			})
+			.catch((err) => {
+				console.error('There was an error while fetching language data: ', err);
+			})
+			.finally(async () => {
+				pageLoading = false;
+				// https://svelte.dev/tutorial/tick
+				await tick();
+				// Set navigation
+				Navigation.selectFirstElement();
+			});
 
 		// Fetch recent conversations
 		fetchRecentConversations();
@@ -75,6 +100,8 @@
 	};
 
 	const handleKeyDown = (event: KeyboardEvent) => {
+		// Prevent default behavior of arrow keys when page is loading
+		if (pageLoading) return;
 		switch (event.key) {
 			case 'Enter':
 				// Handle selecting a contact
@@ -92,76 +119,77 @@
 	};
 </script>
 
-<main class="h-full">
-	<ul>
-		<li>
-			<ContactComponent
-				src="/uttrr.svg"
-				phone={usersPhone}
-				datetime={userMessageDateTime}
-				count={userMessageCount}
-			/>
-		</li>
-	</ul>
-	<div class="flex items-center space-x-2 bg-divider px-2 py-1 text-white">
-		<img src="/clock.svg" alt="clock" class="h-3.5 w-3.5" />
-		<h2 class="text-sm">Recent</h2>
-	</div>
-	<ul>
-		{#each recentConversations as conversation}
+{#if !pageLoading}
+	<main class="h-full">
+		<ul>
 			<li>
 				<ContactComponent
 					src="/uttrr.svg"
-					phone={conversation.mobile_number}
-					datetime={conversation.last_message_at}
-					count={conversation.unread_count}
+					phone={usersPhone}
+					datetime={userMessageDateTime}
+					count={userMessageCount}
 				/>
 			</li>
-		{/each}
-	</ul>
-	<div class="flex items-center space-x-2 bg-divider px-2 py-1 text-white">
-		<img src="/contacts.svg" alt="clock" class="h-3.5 w-3.5" />
-		<h2 class="text-sm">Contacts</h2>
-	</div>
-	<ul>
-		<li>
-			<ContactComponent
-				src="/uttrr.svg"
-				phone="9899272513"
-				datetime="2021-01-01 12:00 AM"
-				count={0}
-			/>
-		</li>
-		<li>
-			<ContactComponent
-				src="/uttrr.svg"
-				phone="9844641240"
-				datetime="2021-01-01 12:00 AM"
-				count={0}
-			/>
-		</li>
-		<li>
-			<ContactComponent
-				src="/uttrr.svg"
-				phone="8879911320"
-				datetime="2021-01-01 12:00 AM"
-				count={0}
-			/>
-		</li>
-		<li>
-			<ContactComponent
-				src="/uttrr.svg"
-				phone="7760032885"
-				datetime="2021-01-01 12:00 AM"
-				count={0}
-			/>
-		</li>
-	</ul>
-	<SoftwareKeys>
-		<div slot="center" class="flex justify-center">
-			<img class="h-4 w-4" src="/tick.svg" alt="ok" />
+		</ul>
+		<div class="flex items-center space-x-2 bg-divider px-2 py-1 text-white">
+			<img src="/clock.svg" alt="clock" class="h-3.5 w-3.5" />
+			<h2 class="text-sm">{translations?.recent ?? 'Recent'}</h2>
 		</div>
-	</SoftwareKeys>
-</main>
-
+		<ul>
+			{#each recentConversations as conversation}
+				<li>
+					<ContactComponent
+						src="/uttrr.svg"
+						phone={conversation.mobile_number}
+						datetime={conversation.last_message_at}
+						count={conversation.unread_count}
+					/>
+				</li>
+			{/each}
+		</ul>
+		<div class="flex items-center space-x-2 bg-divider px-2 py-1 text-white">
+			<img src="/contacts.svg" alt="clock" class="h-3.5 w-3.5" />
+			<h2 class="text-sm">{translations?.contacts ?? 'Contacts'}</h2>
+		</div>
+		<ul>
+			<li>
+				<ContactComponent
+					src="/uttrr.svg"
+					phone="9899272513"
+					datetime="2021-01-01 12:00 AM"
+					count={0}
+				/>
+			</li>
+			<li>
+				<ContactComponent
+					src="/uttrr.svg"
+					phone="9844641240"
+					datetime="2021-01-01 12:00 AM"
+					count={0}
+				/>
+			</li>
+			<li>
+				<ContactComponent
+					src="/uttrr.svg"
+					phone="8879911320"
+					datetime="2021-01-01 12:00 AM"
+					count={0}
+				/>
+			</li>
+			<li>
+				<ContactComponent
+					src="/uttrr.svg"
+					phone="7760032885"
+					datetime="2021-01-01 12:00 AM"
+					count={0}
+				/>
+			</li>
+		</ul>
+		<SoftwareKeys>
+			<div slot="center" class="flex justify-center">
+				<img class="h-4 w-4" src="/tick.svg" alt="ok" />
+			</div>
+		</SoftwareKeys>
+	</main>
+{/if}
 <svelte:window on:keydown={handleKeyDown} />
