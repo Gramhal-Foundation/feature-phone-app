57ec51fb1056171f6cad7e90e0c4ea45d750d9c2
akashrajum-gramhal <akash@gramhal.org>
2023-01-18 18:22:49 +0530

    add translations to preview page

--- src/routes/messages/[phone]/preview/[localMessageId]/+page.svelte
+++ src/routes/messages/[phone]/preview/[localMessageId]/+page.svelte
@@ -6,13 +6,35 @@
 	import SoftwareKeys from '$lib/components/SoftwareKeys.svelte';
 	import { goto } from '$app/navigation';
 	import axiosInstance from '$lib/utils/axios';
+	import { getLocale } from '$lib/utils';
+
+	interface Translations {
+		sending: string;
+		sent: string;
+		sendError: string;
+	}
 
 	let player: HTMLAudioElement | null = null;
 	let playing = false;
 	let audioBlob: Blob | null = null;
 	let time = '0:0 / 0:0';
+	let pageLoading: boolean = true;
+	let translations: Translations | null = null;
 
 	onMount(() => {
+		const lang = getLocale();
+		pageLoading = true;
+		axiosInstance
+			.get(`translations/${lang}/preview`)
+			.then((res) => {
+				translations = res.data;
+			})
+			.catch((err) => {
+				console.error('There was an error while fetching language data: ', err);
+			})
+			.finally(async () => {
+				pageLoading = false;
+			});
 		// Get the audio blob from messages collection in uttrr db in indexdb
 		let db: IDBDatabase;
 		const request = indexedDB.open('uttrr', 1);
@@ -88,12 +110,14 @@
 				);
 				goto('/home');
 			} catch (error) {
-				toast.error('Error sending recording');
+				toast.error(translations?.sendError ?? 'Error sending message');
 			}
 		}
 	};
 
 	const handleKeyDown = (event: KeyboardEvent) => {
+		// Prevent actions when page is loading
+		if (pageLoading) return;
 		switch (event.key) {
 			case 'ArrowLeft':
 			case 'SoftLeft':
@@ -122,42 +146,44 @@
 					player.currentTime = 0;
 				}
 				toast.promise(sendRecording(), {
-					loading: 'Sending recording',
-					success: 'Recording sent',
-					error: 'Error sending recording'
+					loading: translations?.sending ?? 'Sending...',
+					success: translations?.sent ?? 'Sent!',
+					error: translations?.sendError ?? 'Error sending message'
 				});
 				break;
 		}
 	};
 </script>
 
-<main class="flex h-screen flex-col bg-primary">
-	<section class="flex flex-grow flex-col items-center space-y-4 py-4 px-4">
-		<div class="flex flex-col items-center">
-			<img src="/uttrr.svg" alt="logo" class="h-16 w-16" />
-			<h1 class="text-center text-lg font-semibold text-white">{$page.params.phone}</h1>
-		</div>
+{#if !pageLoading}
+	<main class="flex h-screen flex-col bg-primary">
+		<section class="flex flex-grow flex-col items-center space-y-4 py-4 px-4">
+			<div class="flex flex-col items-center">
+				<img src="/uttrr.svg" alt="logo" class="h-16 w-16" />
+				<h1 class="text-center text-lg font-semibold text-white">{$page.params.phone}</h1>
+			</div>
 
-		<img src="/speaker.svg" alt="speaker" class="h-12 w-12" class:animate-pulse={playing} />
+			<img src="/speaker.svg" alt="speaker" class="h-12 w-12" class:animate-pulse={playing} />
 
-		<span class="text-white">{time}</span>
-	</section>
-	<SoftwareKeys>
-		<div slot="left" class="flex items-center justify-start">
-			<img class="h-4 w-4" src="/cancel.svg" alt="back" />
-		</div>
-		<div slot="center" class="flex items-center justify-center">
-			{#if playing}
-				<img class="h-4 w-4" src="/pause.svg" alt="pause" />
-			{:else}
-				<img class="h-4 w-4" src="/speaker.svg" alt="play" />
-			{/if}
-		</div>
-		<div slot="right" class="flex items-center justify-end">
-			<img class="h-4 w-4" src="/tick.svg" alt="ok" />
-		</div>
-	</SoftwareKeys>
-</main>
+			<span class="text-white">{time}</span>
+		</section>
+		<SoftwareKeys>
+			<div slot="left" class="flex items-center justify-start">
+				<img class="h-4 w-4" src="/cancel.svg" alt="back" />
+			</div>
+			<div slot="center" class="flex items-center justify-center">
+				{#if playing}
+					<img class="h-4 w-4" src="/pause.svg" alt="pause" />
+				{:else}
+					<img class="h-4 w-4" src="/speaker.svg" alt="play" />
+				{/if}
+			</div>
+			<div slot="right" class="flex items-center justify-end">
+				<img class="h-4 w-4" src="/tick.svg" alt="ok" />
+			</div>
+		</SoftwareKeys>
+	</main>
+{/if}
 
 <audio class="invisible" />
 
